7eca104513bb9372f6f26d540404df06dc2cfa0c
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-05-10 15:48:55 -0600

    prueba

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -4,7 +4,7 @@ import Publico from "../routes/Publico";
 
 function AppRouter() {
   const { autentificado, cargando, setAutentificado } = useAuth();
-
+  console.log("API:", import.meta.env.VITE_API_URL);
   if (cargando) {
     return null;
   }
