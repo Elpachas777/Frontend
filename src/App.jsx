f7998e2db854fda6391a40c6deaeb1f25d8bcebf
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-05-10 14:25:55 -0600

    Revert "Merge branch 'main' into Fusion"
    This reverts commit d03d9c3d4a0c64e189efb9a217549c0a45cc5b50, reversing
    changes made to 66cdc70e61256f92c877d7ea184c3272ad6c4cde.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import AppRouter from "./routes/AppRouter";
 import "./App.css";
+import AppRouter from "./routes/AppRouter";
 function App() {
   return (
     <Router>
